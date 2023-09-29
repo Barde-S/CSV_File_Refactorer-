21fe059cdd9345fc7552ae74d01f0fc572db2db2
Shuaibu Sani Barde <100693744+Barde-S@users.noreply.github.com>
2023-09-28 19:19:41 -0700

    Update newapp.py

--- newapp.py
+++ newapp.py
@@ -14,7 +14,7 @@ def clean_column_names(input_file, encoding='utf-8'):
     df.columns = df.columns.str.strip()  # Remove trailing spaces
     df.columns = df.columns.str.replace(' ', '')  # Replace spaces with underscores
     df.columns = df.columns.str.lower()
-    df["source"] = df["source"].fillna("https://drive.google.com/drive/folders/1YIIn2o5s3933XyqMirCmiHSxoePYb_nq?usp=share_link")
+    df["source"] = "https://drive.google.com/drive/folders/1YIIn2o5s3933XyqMirCmiHSxoePYb_nq?usp=share_link"
 
 
 
