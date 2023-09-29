e5794b30fe511b2ffa74b1db818af204cd4625b5
Shuaibu Sani Barde <100693744+Barde-S@users.noreply.github.com>
2023-09-28 19:35:35 -0700

    Update newapp.py

--- newapp.py
+++ newapp.py
@@ -9,7 +9,7 @@ def clean_column_names(input_file, encoding='utf-8'):
     # Read the CSV file into a DataFrame with the specified encoding
     df = pd.read_csv(input_file, encoding=encoding)
 
-
+# Check for duplicated columns and drop the  unwanted columns 
     duplicates = set()
     columns_to_drop = []
 
