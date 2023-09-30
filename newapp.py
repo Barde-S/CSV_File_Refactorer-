bbb40d158313f9c3e8ee8358d40098f5a8c131e6
Shuaibu Sani Barde <100693744+Barde-S@users.noreply.github.com>
2023-09-30 06:58:37 +0100

    Update newapp.py

--- newapp.py
+++ newapp.py
@@ -40,7 +40,8 @@ def clean_column_names(input_file, encoding='utf-8'):
 
     df["source"] = "https://drive.google.com/drive/folders/1YIIn2o5s3933XyqMirCmiHSxoePYb_nq?usp=share_link"
     cols = {'practicearea': 'practice_name', 
-             'phonenumber':'phone'}
+             'phonenumber':'phone',
+             'address1':'address'}
     df = df.rename(columns=cols)
 
     return df
